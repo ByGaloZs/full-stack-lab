5cfdce3eb7c193652e642d8e42bad0cb3ac345b0
Mario Padilla <mario-franco97@hotmail.com>
2025-10-12 23:53:03 -0600

    Add Heading Functionality for Todo List

--- todo_list/src/Components/TodoList.jsx
+++ todo_list/src/Components/TodoList.jsx
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
-  
+
+  const [todos, setTodos] = useState([]);
+  const [headingInput, setHeadingInput] = useState('');
+  const [listInputs, setListInputs] = useState({});
+
+  const handleAddTodo = () => {
+    if (headingInput.trim() !== ''){
+      setTodos([...todos, { heading: headingInput, listd: []}]);
+      setHeadingInput('');
+    };
+  }
 
   return (
     <>
