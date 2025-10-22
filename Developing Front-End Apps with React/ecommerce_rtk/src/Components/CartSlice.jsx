cf1eb5688b50b317f167b59378123c0d7fdbc718
Mario Padilla <mario-franco97@hotmail.com>
2025-10-21 22:24:37 -0600

    Display products in shopping cart

--- Developing Front-End Apps with React/ecommerce_rtk/src/Components/CartSlice.jsx
+++ Developing Front-End Apps with React/ecommerce_rtk/src/Components/CartSlice.jsx
@@ -1,17 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
-
 const initialState = {
   cartItems: [],
 };
-
 const CartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
     addItemToCart(state, action) {
-      const existingItem = state.cartItems.find(
-        (item) => item.id === action.payload.id
-      );
+      const existingItem = state.cartItems.find((item) => item.id === action.payload.id);
       if (existingItem) {
         existingItem.quantity += 1;
       } else {
@@ -19,25 +15,19 @@ const CartSlice = createSlice({
       }
     },
     removeItemFromCart(state, action) {
-      state.cartItems = state.cartItems.filter(
-        (item) => item.id !== action.payload
-      );
+      state.cartItems = state.cartItems.filter((item) => item.id !== action.payload);
     },
     clearCart(state) {
       state.cartItems = [];
     },
     increaseItemQuantity(state, action) {
-      const itemToIncrease = state.cartItems.find(
-        (item) => item.id === action.payload
-      );
+      const itemToIncrease = state.cartItems.find((item) => item.id === action.payload);
       if (itemToIncrease) {
         itemToIncrease.quantity += 1;
       }
     },
     decreaseItemQuantity(state, action) {
-      const itemToDecrease = state.cartItems.find(
-        (item) => item.id === action.payload
-      );
+      const itemToDecrease = state.cartItems.find((item) => item.id === action.payload);
       if (itemToDecrease && itemToDecrease.quantity > 1) {
         itemToDecrease.quantity -= 1;
       }
