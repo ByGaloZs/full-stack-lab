24feda8cfdbc356a8f9676d90600bf2da28f4c8d
Mario Padilla <mario-franco97@hotmail.com>
2025-10-27 23:11:36 -0600

    Deploy using GitHub Pages

--- Developing Front-End Apps with React/e-plantShopping/vite.config.js
+++ Developing Front-End Apps with React/e-plantShopping/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: '/full-stack-lab/Developing%20Front-End%20Apps%20with%20React/e-plantShopping',
   plugins: [react()],
-})
+});
