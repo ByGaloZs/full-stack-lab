119ec296513bee7d9e0b78d20eb8f1d82560c398
Mario Padilla <mario-franco97@hotmail.com>
2026-02-01 22:36:31 -0600

    Hands on Lab done

---
+++ 04-Developing AI Applications with Python and Flask/03-Hands-on Lab_Practice Session and Assignment on Python Packaging/mymath/geometry.py
@@ -0,0 +1,17 @@
+import math
+def area_of_rectangle(b, h):
+    """
+    This function returns the area of a rectangle
+    
+    :param b: base (with) of the rectangle
+    :param h: Height of the rectangle
+    """
+    return(b * h)
+
+def area_of_circle(r):
+    """
+    This function returns the area of a circle
+    
+    :param r: radius of the circle
+    """
+    return((math.pi)*(r**2))
